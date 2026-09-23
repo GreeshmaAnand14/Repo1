f73963448b92161f9ae061a734cb9f1f456718a3
GreeshmaAnand14 <greeshgreesh12@gmail.com>
2026-09-23 18:00:04 +0000

    Added counting odd or even code

---
+++ c_codes/file2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers:\n", n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    printf("\n----- Result -----\n");
+    printf("Even numbers = %d\n", even);
+    printf("Odd numbers = %d\n", odd);
+
+    return 0;
+}
